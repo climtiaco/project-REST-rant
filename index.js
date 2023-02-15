e90c612d8aa72113c1a59bd5e1af8beb4d56f44e
Christian Limtiaco <114689190+climtiaco@users.noreply.github.com>
2023-02-14 21:02:56 -0800

    Issues with app crashing

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 require('dotenv').config()
 
+//
 const express = require('express')
 const app = express()
 
-app.use('/places', require('/controllers/places'))
+// Also if I uncomment this part, the app crashes
+//app.use('/places', require('/controllers/places'))
 
 // Home Page
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-// Page Not Found
-app.get('*', (req, res) => {
-    res.status(404).send(
-        <h1>404 Page</h1>
-    )
-})
+//Page Not Found .... Found out that this is what's making my page crash
+// app.get('*', (req, res) => {
+//      res.status(404).send(<h1>404 Page</h1>)
+// })
 app.listen(process.env.PORT)
 
