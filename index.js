f2294f0fb796d6110c27585ce73075ba68e876f5
Christian Limtiaco <114689190+climtiaco@users.noreply.github.com>
2023-02-15 11:48:35 -0800

    added README.md and ran postman to find my local host page, successfully

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express()
 
 // Also if I uncomment this part, the app crashes
 // 2.15.23 This is still breaking my app right now so not sure quite yet whats happening. Tried going into my places.js to add more backticks to see if that was the issue but it was not
-// app.use('/places', require('/controllers/places'))
+// Figured it out again, I forgot to put the "period" in "./controllers/places" so now I understand this part better because its tracking where the route is going.
+app.use('/places', require('./controllers/places'))
 
 // Home Page
 app.get('/', (req, res) => {
