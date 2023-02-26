90d8d96f73974dadce0b7ef24c0ed367ddad0ae7
Christian Limtiaco <114689190+climtiaco@users.noreply.github.com>
2023-02-26 12:41:41 -0800

    jazzed up form page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,10 +6,34 @@ function new_form () {
         <Def>
             <main>
                 <h1>Add a New place</h1>
-                <form method="POST" action="/places">
-                    <div>
-                        <label>Place Name</label>
+                <form method="GET" action="/places">
+                    <div className="form-group">
+                        <label>Place Name </label>
+                        <input className="form-control" id="name" name="name" required />
                     </div>
+                    <br />
+                    <div className="form-group">
+                        <label htmlFor="pic">Place Picture</label>
+                        <input className="form-control" type="url" id="pic" name="pic" />
+                    </div>
+                    <br />
+                    <div className="form-group">
+                        <label htmlFor="city">Place City</label>
+                        <input className="form-control" type="text "id="city" name="city" />
+                    </div>
+                    <br />
+                    <div className="form-group">
+                    <label htmlFor="state">Place State</label>
+                        <input className="form-control" type="text "id="state" name="state" />
+                    </div>
+                    <br />
+                    <div className="form-group">
+                    <label htmlFor="cuisines">Cuisines</label>
+                        <input className="form-control" type="text "id="cuisines" name="cuisines" required/>
+                    </div>
+                    <br />
+                    <input className="btn btn-primary" type="submit" value="Add Place" />
+                    {/* Dont understand how the submit button rerouted to the '/places' page */}
                 </form>
             </main>
         </Def>
